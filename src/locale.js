5753cfc7a16be65e73ebc7b8ebe8445d67e06695
fabioricali <ricalifabio@gmail.com>
2018-01-03 18:05:57 +0100

    remove ref

--- src/locale.js
+++ src/locale.js
@@ -1,10 +1,10 @@
 module.exports = {
     UNKNOWN_TYPE: 'Unknown type: "{type}"',
     TYPE_FAIL: '"{path}" expects "{type}" type but receives: {dataField}',
-    VALIDATOR_FAIL: '"{path}" fail, "{validator}" returns false',
     TYPE_ARRAY_FAIL: '"{path}" expects array of "{type}" type but receives: {dataField}',
     TYPE_FUNCTION_FAIL: '"{path}" receives: {dataField}',
     FIELD_REQUIRED: '"{path}" is required',
     FIELD_CANNOT_EMPTY: '"{path}" cannot be empty',
-    DATA_REQUIRED: 'Data is required and must be an object'
+    DATA_REQUIRED: 'Data is required and must be an object',
+    UNKNOWN_DETECTED: 'Unknown fields were detected: {unknown}'
 };
