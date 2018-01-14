62a8e3ea787ab56e16a424d91fbfe5c4b5eb0049
Fabio Ricali <ricalifabio@gmail.com>
2018-01-14 12:50:09 +0100

    v4.0.2

--- dist/valify.js
+++ dist/valify.js
@@ -1,4 +1,4 @@
-// [AIV]  Valify Build version: 4.0.1  
+// [AIV]  Valify Build version: 4.0.2  
  (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
